78b91281cee19cb241b032e67ff217f4e2e15ceb
HP <HP@LAPTOP-VM1UM6F7>
2020-04-07 20:35:17 +0530

    second commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# projectsampe1
--- src/test/java/com/telusko/demo/MywebappApplicationTests.java
+++ src/test/java/com/telusko/demo/MywebappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.infy.demo;
+package com.telusko.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Application1ApplicationTests {
+class MywebappApplicationTests {
 
 	@Test
 	void contextLoads() {

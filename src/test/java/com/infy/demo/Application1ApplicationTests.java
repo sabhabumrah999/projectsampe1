3c9233f65592af7223db20855a2f45f0d3225f57
HP <HP@LAPTOP-VM1UM6F7>
2020-04-07 20:26:10 +0530

    first commit

---
+++ src/test/java/com/infy/demo/Application1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
